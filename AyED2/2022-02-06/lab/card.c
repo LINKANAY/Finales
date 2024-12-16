e28bf25677f1809ec4130a1b4919020a3d0466c4
Sebastian Sierra <sebastian.sierra@mi.unc.edu.ar>
2024-12-15 23:30:32 -0300

    update

--- AyED2/2022-02-06/lab/card.c
+++ AyED2/2022-02-06/lab/card.c
@@ -1,20 +1,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <stdio.h>
 #include <assert.h>
 #include "card.h"
 
-struct s_card {
+struct s_card
+{
     unsigned int num;
     color_t color;
     type_t type;
 };
 
 static bool
-invrep(card_t c) {
+invrep(card_t c)
+{
     // Invariante de representación
-    bool valid=false;
-    valid = c != NULL;
     /*
      * [COMPLETAR]
      *
@@ -22,72 +22,126 @@ invrep(card_t c) {
      * propiedades de los campos de la estructura
      *
      */
-    return valid;
+    if (c == NULL) return false;
+
+    bool valid_color = (c->color == red || c->color == green || 
+                        c->color == blue || c->color == yellow);
+    bool valid_type = (c->type == normal || c->type == change_color || 
+                        c->type == skip);
+    bool valid_num = (c->num < 10); // Sólo cartas normales tienen número
+
+    return valid_color && valid_type && valid_num;
 }
 
 card_t
-card_new(unsigned int num, color_t color, type_t s) {
-    card_t card=NULL;
+card_new(unsigned int num, color_t color, type_t s)
+{
+    card_t card = NULL;
     /*
      * [COMPLETAR]
      *
      */
+    if (num >= 10 || !(color == red || color == green || color == blue || color == yellow) ||
+        !(s == normal || s == change_color || s == skip)) {
+        fprintf(stderr, "Error: Valores inválidos para la carta.\n");
+        return NULL;
+    }
+
+    card = malloc(sizeof(struct s_card));
+    if (card == NULL) {
+        perror("Error al asignar memoria para la carta");
+        exit(EXIT_FAILURE);
+    }
+    card->num = num;
+    card->color = color;
+    card->type = s;
+
     return card;
 }
 
-
 type_t
-card_get_number(card_t card) {
-    assert(invrep(card));
+card_get_number(card_t card)
+{
+    //assert(invrep(card));
     return card->num;
 }
 
 color_t
-card_get_color(card_t card) {
-    assert(invrep(card));
+card_get_color(card_t card)
+{
+    //assert(invrep(card));
     return card->color;
 }
 
 type_t
-card_get_type(card_t card) {
-    assert(invrep(card));
+card_get_type(card_t card)
+{
+    if (card == NULL) {
+        fprintf(stderr, "Error: card_get_type recibió un puntero NULL.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //assert(invrep(card));
     return card->type;
 }
 
-bool
-card_samecolor(card_t c1, card_t c2) {
+bool card_samecolor(card_t c1, card_t c2)
+{
     /*
      * [COMPLETAR]
      *
      */
+    return (c1->color == c2->color);
 }
 
-
-bool
-card_samenum(card_t c1, card_t c2) {
+bool card_samenum(card_t c1, card_t c2)
+{
     /*
      * [COMPLETAR]
      *
      */
+return (c1->num == c2->num);
 }
 
-bool
-card_compatible(card_t new_card, card_t pile_card) {
-    bool compatible=false;
+bool card_compatible(card_t new_card, card_t pile_card)
+{
+    bool compatible = false;
     assert(invrep(new_card) && invrep(pile_card));
     /*
      * [COMPLETAR]
      *
      */
+    if (new_card->color == pile_card->color)
+    {
+        compatible = true;
+    }
+    if(new_card->type == change_color)
+    {
+        compatible = true;
+    }
+    if((new_card->type == normal && pile_card->type == normal) &&( 
+    (new_card->color == pile_card->color) || new_card->num == pile_card->num))
+    {
+        compatible = true;
+    }
+    if(new_card->type == skip && 
+        (pile_card->type == skip || pile_card->color == new_card->color) )
+    {
+        compatible = true;
+    }
     return compatible;
 }
 
 card_t
-card_destroy(card_t card) {
+card_destroy(card_t card)
+{
     /*
      * [COMPLETAR]
      *
      */
+    if (card != NULL) {
+        free(card);
+        card = NULL; // Asegurar que el puntero no quede colgando
+    }
+    return card;
 }
-
-
--- AyED2/2022-02-06/lab/unogame.c
+++ AyED2/2022-02-06/lab/unogame.c
@@ -1,90 +1,179 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <stdio.h>
 
 #include "unogame.h"
 #include "card.h"
 
 #define NO_PLAYER -1
 
-struct s_ugame {
+struct s_ugame
+{
     /*
      * [COMPLETAR]
      *
      */
+    card_t card;
+    player_t player;
+    struct s_ugame *next;
+    unsigned int size;
 };
 
 unogame_t
-uno_newgame(card_t card) {
+uno_newgame(card_t card)
+{
     /*
      * [COMPLETAR]
      *
      */
+    unogame_t newGame = malloc(sizeof(struct s_ugame));
+    newGame->card = card;
+    newGame->player = NO_PLAYER;
+    newGame->next = NULL;
+    newGame->size = 1;
+    return newGame;
 }
 
 card_t
-uno_topcard(unogame_t uno) {
+uno_topcard(unogame_t uno)
+{
     /*
      * [COMPLETAR]
      *
      */
+    return uno->card;
 }
 
 unogame_t
-uno_addcard(unogame_t uno, card_t c) {
+uno_addcard(unogame_t uno, card_t c)
+{
     /*
      * [COMPLETAR]
      *
      */
+    if (c == NULL)
+    {
+        fprintf(stderr, "Error: No se puede agregar una carta nula a la pila.\n");
+        return uno;
+    }
+
+    struct s_ugame *newCard = malloc(sizeof(struct s_ugame));
+    if (newCard == NULL)
+    {
+        perror("Error al asignar memoria para el nodo");
+        exit(EXIT_FAILURE);
+    }
+    newCard->card = c;
+    newCard->player = uno_nextplayer(uno);
+    newCard->next = uno;
+    newCard->size = uno->size + 1;
+
+    return newCard;
 }
 
 unsigned int
-uno_count(unogame_t uno) {
+uno_count(unogame_t uno)
+{
     /*
      * [COMPLETAR]
      *
      */
+    struct s_ugame *current = uno;
+    unsigned int size = 0;
+    while (current != NULL)
+    {
+        size++;
+        current = current->next;
+    }
+
+    return size;
 }
 
-bool
-uno_validpile(unogame_t uno) {
+bool uno_validpile(unogame_t uno)
+{
     /*
      * [COMPLETAR]
      *
      */
+    if (uno == NULL || uno->next == NULL)
+        return true; // Una carta siempre es válida
+
+    
+    struct s_ugame *current = uno;
+    while (current->next != NULL)
+    {
+        if (!card_compatible(current->card, current->next->card))
+        {
+            return false;
+        }
+        current = current->next;
+    }
+    return true;
 }
 
 color_t
-uno_currentcolor(unogame_t uno) {
+uno_currentcolor(unogame_t uno)
+{
     /*
      * [COMPLETAR]
      *
      */
+    return card_get_color(uno->card);
 }
 
 player_t
-uno_nextplayer(unogame_t uno) {
+uno_nextplayer(unogame_t uno)
+{
     /*
      * [COMPLETAR]
      *
      */
+    assert(uno != NULL);
+    if (card_get_type(uno->card) == skip)
+    {
+        return (uno->player + 2) % NUM_PLAYERS; // Salta un jugador
+    }
+    return (uno->player + 1) % NUM_PLAYERS;
 }
 
-
-
 card_t *
-uno_pile_to_array(unogame_t uno) {
+uno_pile_to_array(unogame_t uno)
+{
     /*
      * [COMPLETAR]
      *
      */
+    card_t *array = malloc(uno->size * sizeof(card_t));
+    if (array == NULL)
+    {
+        perror("Error array: al asignar memoria");
+        exit(EXIT_FAILURE);
+    }
+    struct s_ugame *current = uno;
+    unsigned int len = uno_count(uno);
+    for (int i = len - 1; i >= 0; --i)
+    {
+        array[i] = current->card;
+        current = current->next;
+    }
+    return array;
 }
 
 unogame_t
-uno_destroy(unogame_t uno) {
+uno_destroy(unogame_t uno)
+{
     /*
      * [COMPLETAR]
      *
      */
+    while (uno != NULL) {
+        struct s_ugame *tmp = uno;
+        card_destroy(uno->card); // Libera la carta
+        uno = uno->next;
+        free(tmp);
+    }
+    free(uno);
+    uno = NULL;
+    return uno;
 }
-
